752e0392e84b3f66d9d730a88bf007d52ba8c417
Patricio Di Muzio <dimuziop@gmail.com>
2021-12-29 17:05:37 +0100

    my solution

--- src/main/scala/part2abstractMath/Semigroups.scala
+++ src/main/scala/part2abstractMath/Semigroups.scala
@@ -20,10 +20,22 @@ object Semigroups {
   val stringCombination: String = naturalStringSemigroup.combine("Hello ", "There") // concatenation
 
   def reduceInts(list: List[Int]): Int = list.reduce(naturalIntSemigroup.combine)
+
   def reduceStrings(list: List[String]): String = list.reduce(naturalStringSemigroup.combine)
 
   // general API
   def reduceThings[T](list: List[T])(implicit semigroup: Semigroup[T]): T = list.reduce(semigroup.combine)
+
+
+  // TODO 1: support a new type
+  // hint: use the same patter we use with Eq
+  case class Expense(id: Long, amount: Double)
+
+  implicit val expenseSemigroup: Semigroup[Expense] =
+    Semigroup.instance[Expense] {
+      val r = scala.util.Random
+      (a, b) => Expense(r.nextLong(), a.amount + b.amount)
+    }
 
   def main(args: Array[String]): Unit = {
     println(intCombination)
@@ -31,6 +43,7 @@ object Semigroups {
 
     val numbers = (1 to 10).toList
     val strings = List("I'm ", "starting", " to", " like", " semigroups")
+    val expenses = List(Expense(1,58), Expense(2,75), Expense(3, 57))
     // specific API
     println(reduceInts(numbers))
     println(reduceStrings(strings))
@@ -39,9 +52,10 @@ object Semigroups {
     println(reduceThings(numbers)) // compiler inject the implicit Semigroup[Int]
     println(reduceThings(strings)) // compiler inject the implicit Semigroup[String]
     import cats.instances.option._ // compiler will produce an implicit Semigroup[Option[Int]]
-        // combine will produce another option with summed elements
+    // combine will produce another option with summed elements
     val numberOptions: List[Option[Int]] = numbers.map(n => Option(n))
     println(reduceThings(numberOptions))
+    println(reduceThings(expenses))
 
   }
 
