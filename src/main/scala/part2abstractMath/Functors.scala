08a3dace5f030438cef4bf998bca4d89bcfee851
Patricio Di Muzio <dimuziop@gmail.com>
2021-12-30 17:04:22 +0100

    exercise: first soluton

--- src/main/scala/part2abstractMath/Functors.scala
+++ src/main/scala/part2abstractMath/Functors.scala
@@ -47,12 +47,28 @@ object Functors {
   // generalize
   def do10X[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] = functor.map(container)(_ * 10)
 
+  // TODO 1: define your own functor for a binary tree
 
+  // hint: define an object which extends Functor[Tree]
+  trait Tree[+T]
+
+  case class Leaf[+T](value: T) extends Tree[T]
+
+  case class Branch[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
+
+  implicit object TreeFunctor extends Functor[Tree] {
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(v,l,r) => Branch(f(v), map(l)(f), map(r)(f))
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     println(do10X(List(1, 2, 3)))
     println(do10X(Option(4)))
     println(do10X(Try(5)))
+    //println(do10X(Branch(30, Leaf(10), Leaf(50)))) || invariance error
+    println(do10X[Tree](Branch(30, Leaf(10), Leaf(50))))
   }
 
 }
