a339d0a18145b6a61067461497b5cf51196b62e7
Patricio Di Muzio <dimuziop@gmail.com>
2021-12-30 11:10:15 +0100

    exercise3: my solution

--- src/main/scala/part2abstractMath/Monoids.scala
+++ src/main/scala/part2abstractMath/Monoids.scala
@@ -22,16 +22,20 @@ object Monoids {
   //def combineFold[T: Semigroup](list: List[T]): T = list.foldLeft(/** WHAT?*//)(_ |+| _)
 
   // MONOID
+
   import cats.Monoid
+
   val intMonoid: Monoid[Int] = Monoid[Int]
 
   val combinedNumbers: Int = intMonoid.combine(23, 999)
   val zero = intMonoid.empty // 0
 
   import cats.instances.string._ // brng the implicit Monoid[String] in scope
+
   val emptyStr = Monoid[String].empty
 
   import cats.instances.option._
+
   val emptyOption = Monoid[Option[Int]].empty // None
   val combinedOption = Monoid[Option[Int]].combine(Option(2), Option.empty[Int]) // 2
 
@@ -63,10 +67,40 @@ object Monoids {
 
   import cats.instances.map._
 
+  // TODO 3
+  case class ShoppingCart(items: List[String], total: BigDecimal)
+
+  // define your own monoid - Monoid.instance
+
+  implicit val shoppingCartMonoidInstance: Monoid[ShoppingCart] =
+    Monoid.instance[ShoppingCart](ShoppingCart(List(), 0), { (a, b) =>
+      ShoppingCart(a.items ++ b.items, a.total + b.total)
+    })
+
+  def checkout(shoppingCart: List[ShoppingCart]): ShoppingCart =
+    combineFold(shoppingCart)
+
+  val carts1 = List(
+    ShoppingCart(List("Deodorant", "Sugar", "Bleach"), 25),
+    ShoppingCart(List("Codeine", "Lighter", "Pipe"), 34),
+    ShoppingCart(List("Coffee", "Cup", "Heater"), 65),
+  )
+
+  val carts2 = List(
+    ShoppingCart(List("Linter", "Sleeping Bag", "Gas Lamp"), 33),
+    ShoppingCart(List("Iodopoviodone", "Bandages", "Cloth Tape"), 54),
+    ShoppingCart(List("Rice", "Pasta", "Tuna"), 128),
+  )
+  /*val carts = List(
+    carts1,
+    carts2
+  )*/
+
   def main(args: Array[String]): Unit = {
     println(sumLeft == sumRight)
     println(combineFold(numbers))
     println(combineFold(phonebooks))
+    println(combineFold(carts1))
   }
 
 }
