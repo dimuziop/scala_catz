e7caa3e2e47a01cf5cc8ea5e5176b81808faa562
Patricio Di Muzio <dimuziop@gmail.com>
2021-12-30 16:43:17 +0100

    expl: functors gen

---
+++ src/main/scala/part2abstractMath/Functors.scala
@@ -0,0 +1,58 @@
+package part2abstractMath
+
+import scala.util.Try
+
+/**
+ * User: pat
+ * Date: 30/12/21
+ * Time: 15:51
+ */
+object Functors {
+
+  val aModifiedList: List[Int] = List(1, 2, 3).map(_ + 1)
+  val aModifiedOption: Option[Int] = Option(2).map(_ + 1) // Some(3)
+  val aModifiedTry: Try[Int] = Try(2).map(_ + 1) // Success(3)
+
+  // simplified definition
+  trait MyFunctor[F[_]] {
+    def map[A, B](initialValue: F[A])(f: A => B): F[B]
+  }
+
+  import cats.Functor
+  import cats.instances.list._
+
+  val listFunctor: Functor[List] = Functor[List]
+
+  def incrementedNumbers: List[Int] = listFunctor.map(List(1, 2, 3))(_ + 1)
+
+  import cats.instances.option._
+
+  val optionFunctor: Functor[Option] = Functor[Option]
+
+  def incrementedOption: Option[Int] = optionFunctor.map(Option(2))(_ + 1)
+
+  import cats.instances.try_._
+
+  val tryFunctor: Functor[Try] = Functor[Try]
+
+  def incrementedTry: Try[Int] = tryFunctor.map(Try(2))(_ + 1)
+
+  //generalizing the API
+  def do10xList(list: List[Int]): List[Int] = list.map(_ * 10)
+
+  def do10xOption(option: Option[Int]): Option[Int] = option.map(_ * 10)
+
+  def do10xTry(attempt: Try[Int]): Try[Int] = attempt.map(_ * 10)
+
+  // generalize
+  def do10X[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] = functor.map(container)(_ * 10)
+
+
+
+  def main(args: Array[String]): Unit = {
+    println(do10X(List(1, 2, 3)))
+    println(do10X(Option(4)))
+    println(do10X(Try(5)))
+  }
+
+}
