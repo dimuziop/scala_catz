583d01ab7b92ff7618da9358c299c86100b394f4
Patricio Di Muzio <dimuziop@gmail.com>
2021-12-31 09:04:48 +0100

    exercise: problem 1.1 solved

---
+++ src/main/scala/part2abstractMath/Monads.scala
@@ -0,0 +1,22 @@
+package part2abstractMath
+
+/**
+ * User: pat
+ * Date: 31/12/21
+ * Time: 8:56
+ */
+object Monads {
+
+  // lists
+  val numbersList = List(1, 2, 3)
+  val charsList = List('a', 'b', 'c')
+
+  // TODO 1.1 how do you create all combination of (number, char)?
+  val combination1Point1: List[(Int, Char)] = numbersList.flatMap(n => charsList.map(b => (n, b)))
+
+
+  def main(args: Array[String]): Unit = {
+    println(combination1Point1)
+  }
+
+}
