ac5e6485be6404c53f18a52dbecd1febc3f896e5
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-05 14:54:40 +0100

    exercise: monad implementation without tail rec

--- src/main/scala/part2abstractMath/CustomMonads.scala
+++ src/main/scala/part2abstractMath/CustomMonads.scala
@@ -42,7 +42,40 @@ object CustomMonads {
     override def pure[A](x: A): Identity[A] = x
   }
 
+  // harder example
+
+  sealed trait Tree[+A]
+
+  final case class Leaf[+A](value: A) extends Tree[A]
+
+  final case class Branch[+A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  implicit object TreeMonad extends Monad[Tree] {
+
+    override def flatMap[A, B](fa: Tree[A])(f: A => Tree[B]): Tree[B] = fa match {
+      case Leaf(a) => f(a)
+      case Branch(a, b) => Branch(flatMap(a)(f), flatMap(b)(f))
+    }
+
+    //@tailrec
+    override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
+      def stackRec(t: Tree[Either[A, B]]): Tree[B] = t match {
+        case Leaf(Left(v)) => stackRec(f(v))
+        case Leaf(Right(b)) => Leaf(b)
+        case Branch(left, right) => Branch(stackRec(left), stackRec(right))
+      }
+      stackRec(f(a))
+    }
+
+    override def pure[A](x: A): Tree[A] = Leaf(x)
+  }
+
   def main(args: Array[String]): Unit = {
+
+    val tree: Tree[Int] = Branch(Leaf(10), Leaf(20))
+    val changedTree = TreeMonad.flatMap(tree)(v => Branch(Leaf(v + 1), Leaf(v + 2)))
+
+    println(changedTree)
 
   }
 
