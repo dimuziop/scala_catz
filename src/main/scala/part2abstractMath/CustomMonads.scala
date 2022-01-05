e41b42717879fefa2824660355caf4fd0d0a94c2
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-05 14:05:34 +0100

    exercise: first ex create a monad

---
+++ src/main/scala/part2abstractMath/CustomMonads.scala
@@ -0,0 +1,49 @@
+package part2abstractMath
+
+import scala.annotation.tailrec
+
+/**
+ * User: pat
+ * Date: 5/1/22
+ * Time: 13:46
+ */
+object CustomMonads {
+
+  import cats.Monad
+
+  implicit object OptionMonad extends Monad[Option] {
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+
+    // MUST BE Tail recursive by contract
+    @tailrec
+    override def tailRecM[A, B](a: A)(f: A => Option[Either[A, B]]): Option[B] = f(a) match {
+      case None => None
+      case Some(Left(v)) => tailRecM(v)(f)
+      case Some(Right(b)) => Some(b)
+    }
+
+    override def pure[A](x: A): Option[A] = Option(x)
+  }
+
+  // TODO define a monad for the Identity Type
+  type Identity[T] = T
+  val aNumber: Identity[Int] = 42
+
+  implicit object IdentityMonad extends Monad[Identity] {
+    override def flatMap[A, B](fa: Identity[A])(f: A => Identity[B]): Identity[B] =
+      f(fa)
+
+    @tailrec
+    override def tailRecM[A, B](a: A)(f: A => Identity[Either[A, B]]): Identity[B] = f(a) match {
+      case Left(v) => tailRecM(v)(f)
+      case Right(b) => b
+    }
+
+    override def pure[A](x: A): Identity[A] = x
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
