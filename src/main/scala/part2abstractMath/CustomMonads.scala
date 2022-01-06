98bb045b3366b1e866e57bb2d671d7d5ce29412b
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-06 16:26:42 +0100

    exercise: monad implementation with tail rec

--- src/main/scala/part2abstractMath/CustomMonads.scala
+++ src/main/scala/part2abstractMath/CustomMonads.scala
@@ -59,13 +59,49 @@ object CustomMonads {
 
     //@tailrec
     override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
-      def stackRec(t: Tree[Either[A, B]]): Tree[B] = t match {
+      /*def stackRec(t: Tree[Either[A, B]]): Tree[B] = t match {
         case Leaf(Left(v)) => stackRec(f(v))
         case Leaf(Right(b)) => Leaf(b)
         case Branch(left, right) => Branch(stackRec(left), stackRec(right))
-      }
-      stackRec(f(a))
+      }*/
+      /*
+            _____1_____
+         __2__       __3__
+        /     \     /     \
+       L1     R2   R3     R4
+        tr([1], [], []) =
+        tr([3, 2, 1], [1], []) =
+        tr([R4, R3, 3, 2, 1], [3, 1], []) =
+        tr([R3, 3, 2, 1], [3, 1], [B4]) =
+        tr([3, 2, 1], [3, 1], [B3, B4]) =
+        tr([2, 1], [1], [B34]) =
+        tr([R2, L1, 2, 1], [2, 1], [B34]) =
+        tr([L1, 2, 1], [2, 1], [B2, B34]) =
+        tr([R1, 2, 1], [2, 1], [B2, B34]) =
+        tr([2,1], [2, 1], [B1, B2, B34]) =
+        tr([1], [1], [B12, B34]) =
+        tr([], [], [B1234]) =
+        B1234
+       */
+      @tailrec
+      def tailRec(todo: List[Tree[Either[A, B]]], expanded: Set[Tree[Either[A, B]]], done: List[Tree[B]]): Tree[B] =
+        if(todo.isEmpty) done.head
+        else todo.head match {
+          case Leaf(Left(v)) => tailRec(f(v) :: todo.tail, expanded, done)
+          case Leaf(Right(b)) => tailRec(todo.tail, expanded, Leaf(b) :: done)
+          case node @ Branch(left, right) =>
+            if (!expanded.contains(node)) {
+              tailRec(right :: left :: todo, expanded + node, done)
+            } else {
+              val newLeft = done.head
+              val newRight = done.tail.head
+              tailRec(todo.tail, expanded, Branch(newLeft, newRight) :: done.drop(2))
+            }
+        }
+      tailRec(List(f(a)), Set(), List())
     }
+
+
 
     override def pure[A](x: A): Tree[A] = Leaf(x)
   }
