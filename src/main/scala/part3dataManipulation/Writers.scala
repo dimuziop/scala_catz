f14e503af83f915736e43ae7ed515c4fa547cd91
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-10 12:52:21 +0100

    explanation and first excercise

---
+++ src/main/scala/part3dataManipulation/Writers.scala
@@ -0,0 +1,63 @@
+package part3dataManipulation
+
+/**
+ * User: pat
+ * Date: 10/1/22
+ * Time: 12:30
+ */
+object Writers {
+
+  import cats.data.Writer
+
+  // 1 - define them at the start
+
+  val aWriter: Writer[List[String], Int] = Writer(List("Initial value"), 45)
+  // 2 - manipulate them with pure FP
+  val anIncreasedWriter = aWriter.map(_ + 1) // value increases, logs stay the same
+  val aLogsWriter = aWriter.mapWritten(_ :+ "Found something Interesting") // value stay the same, logs change
+  val aWriterWithBoth = aWriter.bimap(_ :+ "Found something Interesting", _ + 1)
+
+  val aWriterWithBoth2 = aWriter.mapBoth { (logs, value) =>
+    (logs :+ "Found Something Interesting", value + 1)
+  }
+
+  import cats.instances.vector._
+
+  // flatMap
+  val writerA = Writer(Vector("Log A1", "Log A2"), 10)
+  val writerB = Writer(Vector("Log B1"), 40)
+  val compositeWriter = for {
+    va <- writerA
+    vb <- writerB
+  } yield va + vb
+
+  // reset the logs
+
+  import cats.instances.list._ // an implicit monoid to set 0 value
+
+  val anEmptyWriter = aWriter.reset
+
+  // 3 - dump either the value or the log
+  val desiredValue = aWriter.value
+  val logs = aWriter.written
+  val (l, v) = aWriter.run
+
+  // TODO 1: rewrite a function which "prints" things and writers
+  def countAndSay(n: Int): Unit = {
+    if (n <= 0) println("Starting")
+    else {
+      countAndSay(n - 1)
+      println(n)
+    }
+  }
+
+  def countAndLog(n: Int): Writer[Vector[String], Int] = ???
+
+
+  def main(args: Array[String]): Unit = {
+
+    println(compositeWriter.run)
+
+  }
+
+}
