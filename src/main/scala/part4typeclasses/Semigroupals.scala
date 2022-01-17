de34073f24216ef73265de2d31140924daf729b2
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-17 18:06:06 +0100

    exercise 2 defined

--- src/main/scala/part4typeclasses/Semigroupals.scala
+++ src/main/scala/part4typeclasses/Semigroupals.scala
@@ -31,17 +31,44 @@ object Semigroupals {
 
   // TODO: implement product with monads
   import cats.Monad
-  import cats.syntax.functor._
   import cats.syntax.flatMap._
+  import cats.syntax.functor._
   def productWithMonads[F[_], A, B](fa: F[A], fb: F[B])(implicit monad: Monad[F]): F[(A, B)] = for {
     a <- fa
     b <- fb
   } yield (a, b)
 
+  // MONADS EXTENDS SEMIGROUPALS
+
+  // example: Validated
+  import cats.data.Validated
+
+  type ErrorsOr[T] = Validated[List[String], T]
+  val validatedSemigroupal = Semigroupal[ErrorsOr] // requires the implicit Semigroup[List[_]]
+  val invalidsCombination = validatedSemigroupal.product(
+    Validated.invalid(List("Something wrong", "something else wrong")),
+    Validated.invalid(List("This can't be right"))
+  )
+
+  type EitherErrorsOr[T] = Either[List[String], T]
+  import cats.instances.either._ // implicit Monad[Either]
+  val eitherSemigroupal = Semigroupal[EitherErrorsOr]
+  val eitherCombination = eitherSemigroupal.product( // in terms of map/flatMap
+    Left(List("Something wrong", "something else wrong")),
+    Left(List("This can't be right"))
+  )
+
+  // Associativity: m.flatMap(f).flatMap(g) == m.flatMap(x => f(x).flatMap(g))
+
+  // TODO 2: define a Semigroupal[List] which does a zip
+  val zipListSemigroupal: Semigroupal[List] =
+
   def main(args: Array[String]): Unit = {
     println(aTupledList)
     println(zippedLists)
     println(productWithMonads(List(1, 2), List("a", "b")))
+    println(invalidsCombination)
+    println(eitherCombination)
   }
 
 }
