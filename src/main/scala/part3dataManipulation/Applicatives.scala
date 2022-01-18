ce6371b538115a36206cec827bd3215de2b8f267
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-18 09:24:30 +0100

    aclarations

--- src/main/scala/part3dataManipulation/Applicatives.scala
+++ src/main/scala/part3dataManipulation/Applicatives.scala
@@ -33,13 +33,17 @@ object Applicatives {
 
 
   // TODO: though experiment
-  def ap[W[_], A, B](wf: W[A => B])(wa: W[A]): W[B] = ???
+  //def ap[W[_], A, B](wf: W[A => B])(wa: W[A]): W[B] = ???
   def productWithApplicatives[W[_], A, B](wa: W[A], wb: W[B])(implicit applicative: Applicative[W]): W[(A, B)] = {
     val functionWrapper: W[B => (A,B)] = applicative.map(wa)(a => (b: B) => (a,b))
-    ap(functionWrapper)(wb)
+    applicative.ap(functionWrapper)(wb)
   }
   //  ap(a => applicative.map(wb)(b => (a, b)))(wa)
   //applicative.map(wa)(a => applicative.map(wb)(b => (a, b)))
+
+  // Applicatives have this ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T]
+  // Applicatives can implement product from Semigroupal
+  // => Applicative extends Semigroupal
 
   def main(args: Array[String]): Unit = {
 
