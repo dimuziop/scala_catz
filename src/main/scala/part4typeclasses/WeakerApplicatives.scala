0bc2d3303bfdb3d862f57dae4c59a34ebd83bf60
Patricio Di Muzio <dimuziop@gmail.com>
2022-01-20 08:45:12 +0100

    exercise1: exercise done

--- src/main/scala/part4typeclasses/WeakerApplicatives.scala
+++ src/main/scala/part4typeclasses/WeakerApplicatives.scala
@@ -13,12 +13,14 @@ object WeakerApplicatives {
   trait MyApply[W[_]] extends Functor[W] with Semigroupal[W] {
     override def product[A, B](fa: W[A], fb: W[B]): W[(A, B)] = {
       val functionWrapper: W[B => (A, B)] = map(fa)(a => (b: B) => (a, b))
-
-      // TODO
-      def mapN[A, B, C](tuple: (W[A], W[B]))(f: (A, B) => C): W[C] =
-
       ap(functionWrapper)(fb)
     }
+
+    // TODO
+    def mapN[A, B, C](tuple: (W[A], W[B]))(f: (A, B) => C): W[C] =
+      map(product(tuple._1, tuple._2)) {
+        case (a, b) => f(a, b)
+      }
 
     // fundamental
     def ap[B, T](wf: W[B => T])(wa: W[B]): W[T]
@@ -30,10 +32,12 @@ object WeakerApplicatives {
 
   import cats.Apply
   import cats.instances.option._ // implicit Apply[Option]
+
   val applyOption = Apply[Option]
   val funcApp = applyOption.ap(Some((x: Int) => x + 1))(Some(2)) // Some(3)
 
   import cats.syntax.apply._ // etension methods from Apply
+
   val tupleOfOptions = (Option(1), Option(2), Option(3))
   val optionOfTuple = tupleOfOptions.tupled // Some((1,2,3))
   val supOptions = tupleOfOptions.mapN(_ + _ + _) // Some(6)
